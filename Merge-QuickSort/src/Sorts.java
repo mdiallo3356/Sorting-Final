f58e3666675c8e3e884f227dd92867cf34266040
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-13 09:23:13 -0500

    Stage 1 struggle

--- Merge-QuickSort/src/Sorts.java
+++ Merge-QuickSort/src/Sorts.java
@@ -1,6 +1,28 @@
 /*Author: Mamadou Diallo
  * February 13, 2018: Half a year of Java has passed. They still don't know I'm confused
  */
+import java.util.Arrays;
 public class Sorts {
-
+	public static String[]MErgeSort(String[]words)
+	{
+		if(words.length == 1)
+		{
+			return words;
+		}
+		String[]Left = Arrays.copyofRange(words,0,(words.length)/2)
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
